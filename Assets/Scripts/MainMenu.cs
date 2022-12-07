8eda502edfc3d0d1704a23079158cb2777221de7
Drewbertlemon13 <stamoura@uwindsor.ca>
2022-12-06 21:46:52 -0500

    New Main Menu with Unity Store Assets

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    const int m_buttonW = 200;
-    const int m_buttonH = 50;
+    public Font menuFont;
+    GUIStyle noBackground;
+
+    int screenH;
+    int screenW;
+
+    int screen = 0;
+
+    private int m_buttonW; //200
+    private int m_buttonH; //50
     public int m_nbButtons = 4;
     Rect[] m_buttonsRect;
 
@@ -15,9 +23,21 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        screenH = Screen.width;
+        screenW = Screen.height;
+
+        m_buttonW = screenW/4;
+        m_buttonH = screenH/14;
+
+        noBackground = new GUIStyle();
+        noBackground.alignment = TextAnchor.MiddleLeft;
+        noBackground.fontSize = m_buttonH - m_buttonH/6;
+        noBackground.normal.textColor = Color.white;
+        noBackground.font = menuFont;
+
         m_buttonsRect = new Rect[m_nbButtons];
         for(int i = 0; i < m_nbButtons; ++i){
-            m_buttonsRect[i] = new Rect(500, (i * (m_buttonH + 25)) + 200, m_buttonW, m_buttonH);
+            m_buttonsRect[i] = new Rect(m_buttonW/6, (i * (m_buttonH + m_buttonH/8)) + screenH/16, m_buttonW, m_buttonH);
         }
 
         m_levelNames = new string[m_nbButtons];
@@ -36,21 +56,26 @@ public class MainMenu : MonoBehaviour
 
     void OnGUI(){
         int i = 0;
-        for(i = 0; i < m_nbButtons; ++i){
-            if(GUI.Button(m_buttonsRect[i], m_levelNames[i])){
-                // When "Quit" is pressed
-                if(i == 3){
-                    // Put cleanup code before game stops execution here
+        if (screen == 0)
+        {
+            for(i = 0; i < m_nbButtons; ++i){
+                if(GUI.Button(m_buttonsRect[i], m_levelNames[i], noBackground)){
+                    // When "Quit" is pressed
+                    if(i == 3){
+                        // Put cleanup code before game stops execution here
 
 
-                    // Quit the game (Ignored in the unity editor only when the game is built)
-                    Application.Quit();
-                }
-                else{
-                    // Load the pressed scene
-                    SceneManager.LoadScene(m_levelNames[i]);
+                        // Quit the game (Ignored in the unity editor only when the game is built)
+                        Application.Quit();
+                    }
+                    else{
+                        // Load the pressed scene
+                        SceneManager.LoadScene(m_levelNames[i]);
+                    }
                 }
             }
         }
+        if (screen == 1)
+        {}
     }
 }
