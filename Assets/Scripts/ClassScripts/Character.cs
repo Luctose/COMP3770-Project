97a491602e2e514e0c6f7457be9fd2afd0717841
Luctose <lucassarweh4@gmail.com>
2022-12-09 17:47:10 -0500

    Made base character class and started files for the different classes

--- Assets/Scripts/ClassScripts/Character.cs
+++ Assets/Scripts/ClassScripts/Character.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Character
 {
-
-    private string className;
-    private string classDesc;
+    // Class info
+    private string className; // Name
+    private string classDesc; // Description
 
     // Stats
     private int level; // Character level
@@ -18,4 +18,64 @@ public class Character
     private int magicDamage; // Combat magic attack
     private int physicalResistance; // Combat physical defense
     private int magicResistance; // Combat magic defense
+
+    // Other character attributes
+    // private Inventory characterInventory; <-- Commented out as the Inventory class needs to be made first
+    // private Item Equipped; <-- Same as above
+
+    // Getters and Setters
+    public string ClassName{
+        get{return className;}
+        set{className = value;}
+    }
+
+    public string ClassDesc{
+        get{return classDesc;}
+        set{classDesc = value;}
+    }
+
+    public int Level{
+        get{return level;}
+        set{level = value;}
+    }
+
+    public int Exp{
+        get{return exp;}
+        set{exp = value;}
+    }
+
+    public int Hp{
+        get{return hp;}
+        set{hp = value;}
+    }
+
+    public int Movement{
+        get{return movement;}
+        set{movement = value;}
+    }
+
+    public int Speed{
+        get{return speed;}
+        set{speed = value;}
+    }
+
+    public int AttackDamage{
+        get{return attackDamage;}
+        set{attackDamage = value;}
+    }
+
+    public int MagicDamage{
+        get{return magicDamage;}
+        set{magicDamage = value;}
+    }
+
+    public int PhysicalResistance{
+        get{return physicalResistance;}
+        set{physicalResistance = value;}
+    }
+
+    public int MagicResistance{
+        get{return magicResistance;}
+        set{magicResistance = value;}
+    }
 }
---
+++ Assets/Scripts/ClassScripts/Swordsman.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordsman : Character
+{
+    /* Constructor
+    */
+    public Swordsman(){
+
+    }
+}
