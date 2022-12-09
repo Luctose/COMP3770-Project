b50899cf2c1dcf9e54d4988b49299b08eee9e5d4
Luctose <lucassarweh4@gmail.com>
2022-12-08 21:26:43 -0500

    Camera not quite yet finished

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // For Zooming in and out
+    float FOV; // Field of View
+    public float maxZm = 80f; // Max zoom
+    public float minZm = 15f; // Min zoom
+    public float sensitivity = 5f; // Zoom scroll sensitivity
+    public int x_cam = -10;
+    public int y_cam = 52;
+    public int z_cam = -5;
+
+    // The Camera
+    Camera theCam;
+
+    // For moving the camera up, down, left, right
+    public Vector2 topLeftOfMap; // Top left corner of the current map
+    public Vector2 bottomRightOfMap; // Bottom right corner of the current map
+
+    Vector2 bottomLeftOfMap; // These calculated from what's given
+    Vector2 topRightOfMap;
+    // Moving using the mouse
+
+    // Moving using the arrow keys
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = this.GetComponent<Camera>();
+        // Set initial rotation
+        this.transform.rotation = Quaternion.Euler(90, transform.rotation.y, transform.rotation.z);
+        // Set initial postion
+        this.transform.position = new Vector3(x_cam, y_cam, z_cam);
+        // Set intial ZOOM
+        FOV = 60.0f;
+        
+        bottomLeftOfMap.Set(topLeftOfMap.x, bottomRightOfMap.y);
+        topRightOfMap.Set(bottomRightOfMap.x, topLeftOfMap.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.mousePosition is (0, 0) to (Screen.width, Screen.height) Use this to move camera w/ mouse
+        // Moving using the mouse
+        //if(Input.mousePosition.x)
+
+        // Moving using the arrow keys
+        if(Input.GetKey("up")){
+            this.transform.Translate(0, 1 * Time.fixedDeltaTime, 0);
+
+        }else if(Input.GetKey("down")){
+            this.transform.Translate(0, -1 * Time.fixedDeltaTime, 0);
+
+        }if(Input.GetKey("left")){
+            this.transform.Translate(-1 * Time.fixedDeltaTime, 0, 0);
+
+        }else if(Input.GetKey("right")){
+            this.transform.Translate(1 * Time.fixedDeltaTime, 0, 0);
+        }
+
+        // Start block for mousewheel to zoom in and out
+        FOV += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        FOV = Mathf.Clamp(FOV, minZm, maxZm);
+        theCam.fieldOfView = FOV;
+        // End
+    }
+}
