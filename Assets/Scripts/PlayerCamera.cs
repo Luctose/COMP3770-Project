461966d6dc421fe74d164c3c554d2319aab6101e
Luctose <lucassarweh4@gmail.com>
2022-12-09 00:13:32 -0500

    Completed PlayerCamera Script and/or Prefab

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,9 +9,11 @@ public class PlayerCamera : MonoBehaviour
     public float maxZm = 80f; // Max zoom
     public float minZm = 15f; // Min zoom
     public float sensitivity = 5f; // Zoom scroll sensitivity
+    // Initialize camera position
     public int x_cam = -10;
     public int y_cam = 52;
     public int z_cam = -5;
+    // Camera slide speed
     public int cam_speed = 3;
 
     // The Camera
@@ -22,7 +24,10 @@ public class PlayerCamera : MonoBehaviour
     public Vector2 bottomRightOfMap; // Bottom right corner of the current map
 
     // Moving using the mouse
-    
+    Vector3 cam_pos; // Used to modify camera position
+    bool lockMouseSlide = false; // Used to lock the camera for mouse sliding
+    int edgeRatio = Screen.width / 10; // 10% of the screen width
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,36 +38,66 @@ public class PlayerCamera : MonoBehaviour
         this.transform.position = new Vector3(x_cam, y_cam, z_cam);
         // Set intial ZOOM
         FOV = 60.0f;
+
+        cam_pos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Input.mousePosition is (0, 0) to (Screen.width, Screen.height) Use this to move camera w/ mouse
-        // Moving using the mouse
-        //if(Input.mousePosition.x)
-
         // Start block for mousewheel to zoom in and out (Do not put in FixedUpdate)
         FOV -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
         FOV = Mathf.Clamp(FOV, minZm, maxZm);
         theCam.fieldOfView = FOV;
         // End
 
+        // Lock/Unlock camera for mouse on key "y"
+        if(Input.GetKeyDown("y")){
+            lockMouseSlide = !lockMouseSlide;
+        }
+
+        // Make cam_pos unable to exceed bounds
+        cam_pos.x = Mathf.Clamp(cam_pos.x, topLeftOfMap.x, bottomRightOfMap.x);
+        cam_pos.z = Mathf.Clamp(cam_pos.z, bottomRightOfMap.y, topLeftOfMap.y);
+        // Update camera position
+        this.transform.position = new Vector3(cam_pos.x, cam_pos.y, cam_pos.z);
     }
 
     void FixedUpdate(){
         // Moving using the arrow keys
-        if(Input.GetKey("w") && this.transform.position.z < topLeftOfMap.y){
-            this.transform.Translate(0, cam_speed * Time.fixedDeltaTime, 0);
+        if(Input.GetKey("w")){
+            cam_pos.z += cam_speed * Time.fixedDeltaTime;
 
-        }else if(Input.GetKey("s") && this.transform.position.z > bottomRightOfMap.y){
-            this.transform.Translate(0, -cam_speed * Time.fixedDeltaTime, 0);
+        }else if(Input.GetKey("s")){
+            cam_pos.z -= cam_speed * Time.fixedDeltaTime;
 
-        }if(Input.GetKey("a") && this.transform.position.x > topLeftOfMap.x){
-            this.transform.Translate(-cam_speed * Time.fixedDeltaTime, 0, 0);
+        }if(Input.GetKey("a")){
+            cam_pos.x -= cam_speed * Time.fixedDeltaTime;
 
-        }else if(Input.GetKey("d") && this.transform.position.x < bottomRightOfMap.x){
-            this.transform.Translate(cam_speed * Time.fixedDeltaTime, 0, 0);
+        }else if(Input.GetKey("d")){
+            cam_pos.x += cam_speed * Time.fixedDeltaTime;
+        }
+
+
+        // Moving using mouse on the edge of the screen
+        // First check if camera is locked
+        if(!lockMouseSlide){
+            // Mouse close to right
+            if(Input.mousePosition.x > Screen.width - edgeRatio){
+                cam_pos.x += cam_speed * Time.fixedDeltaTime;
+            }
+            // Mouse close to left
+            else if(Input.mousePosition.x < edgeRatio){
+                cam_pos.x -= cam_speed * Time.fixedDeltaTime;
+            }
+            // Mouse close to top
+            if(Input.mousePosition.y > Screen.height - edgeRatio){
+                cam_pos.z += cam_speed * Time.fixedDeltaTime;
+            }
+            // Mouse close to bottom
+            else if(Input.mousePosition.y < edgeRatio){
+                cam_pos.z -= cam_speed * Time.fixedDeltaTime;
+            }
         }
     }
 }
