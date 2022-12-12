0b801171ffa08fbc2ca884cc653acd45421241cd
Luctose <lucassarweh4@gmail.com>
2022-12-12 15:36:53 -0500

    Scripts that initialize the Character classes

---
+++ Assets/Scripts/ClassScripts/ArcherDriver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherDriver : MonoBehaviour
+{
+    Character entity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entity = new Archer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ClassScripts/MageDriver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageDriver : MonoBehaviour
+{
+    Character entity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entity = new Mage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
